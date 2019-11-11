93a29f734ca533ed14f5764a4fc55d7840912d97
Dan Seremet <dan_seremet@yahoo.com>
2019-11-10 19:29:38 -0500

    Initial Commit

---
+++ src/main/java/com/soen343/smartdriver/SmartDriverApplication.java
@@ -0,0 +1,13 @@
+package com.soen343.smartdriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartDriverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartDriverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soen343/smartdriver/SmartDriverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soen343.smartdriver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartDriverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
